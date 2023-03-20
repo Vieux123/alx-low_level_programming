4275cad780b77498ba9fb31b0c27d7c3c81e2f3b
Vieux123 <silasiyamu20@gmail.com>
2023-03-20 23:30:24 +0000

    variables if and while in c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -4,24 +4,20 @@
   *Description 'print alphabets except q and e'
   *Return: Always 0
   */
+
 int main(void)
 {
 	int n = 97;
 
 	while (n <= 122)
 	{
-		if (n == 101 || n == 111)
+		if (n == 101 || n == 112)
 		{
-			if (n == 101 || n == 112)
-			{
-				n++;
-				continue;
-			}
-			putchar(n);
 			n++;
+			continue;
 		}
 		putchar(n);
-		n++;
+		n++;	
 	}
 	putchar('\n');
 	return (0);
