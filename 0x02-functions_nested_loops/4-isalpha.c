53a46e71020ef0dfe156157087978db6f3e2900a
Vieux123 <silasiyamu20@gmail.com>
2023-03-21 21:24:01 +0000

    functions, nested loop

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,10 +1,12 @@
 #include "main.h"
-/** _isalpha - Shows 1 if the input is a
- *letter Another cases, shows 0
+
+/**
+ * _isalpha - Shows 1 if the input is a
+ * letter Another cases, shows 0
  *
- *@c: The character in ASCII code
+ * @c: The character in ASCII code
  *
- *Return: 1 for letters. 0 for the rest.
+ * Return: 1 for letters. 0 for the rest.
  */
 int _isalpha(int c)
 {
