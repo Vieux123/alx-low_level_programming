df3e07fd901095432922b6463416e09f3fe5995e
Vieux123 <silasiyamu20@gmail.com>
2023-03-30 21:25:22 +0000

    C - More pointers, arrays and strings

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -1,5 +1,4 @@
 #include "main.h"
-
 /**
   * string_toupper - Function that reverses the content of an array of integers.
   *
@@ -13,10 +12,8 @@ char *string_toupper(char *s)
 
 	while (s[i] != '\0')
 	{
-		if ((s[i] >= 97) && (s[i] <= 122))
-		{
+		if (s[i] >= 'a' && (s[i] <= 'z')
 			s[i] = s[i] - 32;
-		}
 		i++;
 	}
 	return (s);
