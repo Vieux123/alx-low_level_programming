28ea18774306603e3954ebe8c1617cde677e893a
Vieux123 <silasiyamu20@gmail.com>
2023-04-27 16:53:30 +0000

    C - singly linked lists

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -14,9 +14,9 @@ size_t print_list(const list_t *h)
 	while (h)
 	{
 		if (!h->str)
-			print("[0] (nil)\n");
+			printf("[0] (nil)\n");
 		else
-			print("[%u] %s\n", h->len, h->str);
+			printf("[%u] %s\n", h->len, h->str);
 		h = h->next;
 		s++;
 	}
